290a70d41d1d3f4f83d430a91e727d80fd746ea8
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-12-05 21:12:18 +0200

    Add project files.

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lab2ex2
+{
+    /*  Scrieti un program care va afisa factorialul unui numar n,
+        n fiind citit de la tastatura.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Introduceti numarul: ");
+            int n = int.Parse(Console.ReadLine());
+            int fact = 1;
+
+            while (n >= 1)
+            {
+                fact = fact * n;
+                n = n - 1;
+            }
+            Console.WriteLine($"Factorialul numarului ales este: {fact} ");
+        }
+    }
+}
